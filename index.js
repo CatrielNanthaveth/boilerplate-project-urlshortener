56d5a7d5aec40df675624cc2cb8cb676a2172d85
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-06-01 18:43:49 -0300

    terminado

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+// Database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once("open", function() {
+  console.log("we're connected!");
+});
+
+const urlSchema = new mongoose.Schema({
+  id: Number,
+  url: String
+});
+
+const urlModel = mongoose.model('url', urlSchema)
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +37,38 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// URL shorter
+app.post("/api/shorturl", (req, res) => {
+  let urlRegex = /https:\/\/www.|http:\/\/www./g;
+
+  dns.lookup(req.body.url.replace(urlRegex, ''), (err, address, family) => {
+    if(err) {
+      res.json({ error: 'Invalid URL' })
+    } else {
+      urlModel
+        .find().exec().then(data => {
+          new urlModel({
+            id: data.length + 1,
+            url: req.body.url
+          }).save().then(() => {
+            res.json({
+              original_url: req.body.url,
+              short_url: data.length + 1
+            });
+          }).catch(err => {
+            res.json(err);
+          });
+        });
+    }
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  urlModel.find({ id: req.params.id} ).exec().then(url => {
+    res.redirect(url[0]["url"]);
+  });
 });
 
 app.listen(port, function() {
