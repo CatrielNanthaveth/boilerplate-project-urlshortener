18c5d5410e6e49e58392ecf64b7f58d7e1afbc9a
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-06-01 21:04:45 -0300

    deploy
    modificaciones para el deploy en Render

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dns = require('dns');
 const app = express();
 
 // Basic Configuration
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
