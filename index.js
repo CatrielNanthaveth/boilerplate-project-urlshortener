0719dfbb11a0b6b3339e10adf320ac026ef80b7a
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2023-06-02 20:58:13 -0300

    otro intento

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/api/shorturl", (req, res) => {
 });
 
 app.get("/api/shorturl/:id", (req, res) => {
-  urlModel.find({ id: req.params.id} ).exec().then(url => {
+  urlModel.find({ id: Number(req.params.id)} ).exec().then(url => {
     res.redirect(url[0]["url"]);
   });
 });
